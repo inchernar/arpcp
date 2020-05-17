b87295c2dce47b911d5884407bfb690f577fb6f9
julbarys <ilsuryusipov@yandex.ru>
2020-05-17 15:55:42 +0300

    Version 0.6 is ready Added try...except blocks for most instructions, besides redis.get(). Written tests for all ARPCP methods.

--- code/tests/tests.py
+++ code/tests/tests.py
@@ -236,7 +236,7 @@ class TestARPCP:
 			'remote_procedure_args': [2, 'a'],
 			'task_id': task_id
 		})
-		assert response['code'] == 301
+		assert response['code'] == 202
 		assert response['description'] == 'procedure execution error'
 		assert response['data']['result'] == None
 		assert response['data']['task_id'] == task_id
@@ -254,7 +254,7 @@ class TestARPCP:
 			'remote_procedure_args': [1, 2, 3],
 			'task_id': task_id
 		})
-		assert response['code'] == 301
+		assert response['code'] == 202
 		assert response['description'] == 'procedure execution error'
 		assert response['data']['result'] == None
 		assert response['data']['task_id'] == task_id
@@ -273,7 +273,7 @@ class TestARPCP:
 			'remote_procedure_args': [1, 2, 3],
 			'task_id': task_id
 		})
-		assert response['code'] == 301
+		assert response['code'] == 202
 		assert response['description'] == 'procedure execution error'
 		assert response['data']['result'] == None
 		assert response['data']['task_id'] == task_id
@@ -325,109 +325,208 @@ class TestARPCP:
 		assert not self.redis.exists(f'ARPCP:task:{task_id}:message')
 		assert not task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
 
-	def test_result_method_for_correct_task_status(self):
+
+	def test_result_method(self):
 		task_id = str(uuid.uuid4())
-		for status in arpcp.ARPCP.task_statuses:
-			response = arpcp.ARPCP.call(self.host, self.port, 'result', {
-				'task_id': task_id,
-				'task_status': status,
-				'task_result': None
-			})
-			assert response['code'] == 100
-			assert response['description'] == 'OK'
-			assert response['data'] == 'OK'
-			arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
-
-	# def test_result_method_for_incorrect_task_status(self):
-	# 	task_id = str(uuid.uuid4())
-	# 	task_status = 'incorrect_task_status'
-	# 	response = arpcp.ARPCP.call(self.host, self.port, 'result', {
-	# 		'task_id': task_id,
-	# 		'task_status': task_status,
-	# 		'task_result': None
-	# 	})
-	# 	assert response['code'] == 310
-	# 	assert response['description'] == f'task status {task_status} is undefined'
-	# 	assert response['data'] == None
-	# 	arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+		try:
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		except:
+			assigned_tasks = []
+		assigned_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		response = arpcp.ARPCP.call(self.host, self.port, 'result', {
+			'task_id': task_id,
+			'task_status': 'done',
+			'task_result': float(5)
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data'] == 'OK'
+		assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == float(5)
+		assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
 
 
-	# def test_signal_method(self):
-	# 	for status in arpcp.ARPCP.task_statuses:
-	# 		task_id = str(uuid.uuid4())
-	# 		self.redis.set('ARPCP:tasks:execute', json.dumps([task_id]))
-	# 		self.redis.set(f"ARPCP:task:{task_id}:status", status)
-	# 		self.redis.set(f"ARPCP:task:{task_id}:result", json.dumps(None))
-	# 		response = arpcp.ARPCP.call(self.host, self.port, 'signal', {
-	# 			'task_id': task_id
-	# 		})
-	# 		assert response['code'] == 100
-	# 		assert response['description'] == 'OK'
-	# 		assert response['data'] == None
-	# 		time.sleep(0.1)
-	# 		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+	def test_result_method_with_callback(self):
+		task_id = str(uuid.uuid4())
+		try:
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		except:
+			assigned_tasks = []
+		assigned_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		self.redis.set(f'ARPCP:task:{task_id}:callback', 'double')
+		response = arpcp.ARPCP.call(self.host, self.port, 'result', {
+			'task_id': task_id,
+			'task_status': 'done',
+			'task_result': float(5)
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data'] == 'OK'
+		time.sleep(0.2)
+		assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == float(10)
+		assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
+		assert self.redis.get(f'ARPCP:task:{task_id}:callback') == 'double'
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
 
 
-	# def test_signal_method_with_wrong_task_id(self):
-	# 	for status in arpcp.ARPCP.task_statuses:
-	# 		task_id = str(uuid.uuid4())
-	# 		self.redis.set(f"ARPCP:tasks:execute", json.dumps([task_id]))
-	# 		self.redis.set(f"ARPCP:task:{task_id}:status", status)
-	# 		self.redis.set(f"ARPCP:task:{task_id}:result", json.dumps(None))
-	# 		response = arpcp.ARPCP.call(self.host, self.port, 'signal', {
-	# 			'task_id': task_id * 2
-	# 		})
-	# 		assert response['code'] == 315
-	# 		assert response['description'] == 'Non existent task id'
-	# 		assert response['data'] == None
-	# 		time.sleep(0.1)
-	# 		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+	def test_result_method_with_incorrect_callback(self):
+		task_id = str(uuid.uuid4())
+		callback_name = 'incorrect_callback'
+		try:
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		except:
+			assigned_tasks = []
+		assigned_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		self.redis.set(f'ARPCP:task:{task_id}:callback', callback_name)
+		response = arpcp.ARPCP.call(self.host, self.port, 'result', {
+			'task_id': task_id,
+			'task_status': 'done',
+			'task_result': float(5)
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data'] == 'OK'
+		time.sleep(0.2)
+		assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == None
+		assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'callback_error'
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
+		assert self.redis.get(f'ARPCP:task:{task_id}:callback') == callback_name
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
 
 
-	# def test_atask_method_wo_callback(self):
-	# 	task_id = str(uuid.uuid4())
-	# 	response = arpcp.ARPCP.call(self.host, self.port, 'atask', {
-	# 		'remote_procedure': 'add',
-	# 		'remote_procedure_args': [3, 4],
-	# 		'task_id': task_id
-	# 	})
-	# 	assert response['code'] == 100
-	# 	assert response['description'] == 'OK'
-	# 	assert response['data']['task_id'] == task_id
-	# 	assert response['data']['result'] == None
-	# 	time.sleep(0.2)
-	# 	assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == float(3 + 4)
-	# 	assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:message')
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:caller_ip')
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:host_addr')
-	# 	assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
-	# 	assert task_id in json.loads(self.redis.get('ARPCP:tasks:execute'))
-	# 	arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+	def test_result_method_with_callback_and_incorrect_status(self):
+		task_id = str(uuid.uuid4())
+		incorrect_status = None
+		try:
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		except:
+			assigned_tasks = []
+		assigned_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		self.redis.set(f'ARPCP:task:{task_id}:callback', 'double')
+		response = arpcp.ARPCP.call(self.host, self.port, 'result', {
+			'task_id': task_id,
+			'task_status': incorrect_status,
+			'task_result': float(5)
+		})
+		assert response['code'] == 206
+		assert response['description'] == 'incorrect task status'
+		assert response['data'] == None
+		time.sleep(0.2)
+		assert not self.redis.exists(f'ARPCP:task:{task_id}:result')
+		assert not self.redis.exists(f'ARPCP:task:{task_id}:status')
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
 
 
-	# def test_atask_method(self):
-	# 	import callbacks
-	# 	task_id = str(uuid.uuid4())
-	# 	response = arpcp.ARPCP.call(self.host, self.port, 'atask', {
-	# 		'remote_procedure': 'add',
-	# 		'remote_procedure_args': [3, 4],
-	# 		'task_id': task_id
-	# 	}, additions = {
-	# 		'callback': 'double'
-	# 	})
-	# 	assert response['code'] == 100
-	# 	assert response['description'] == 'OK'
-	# 	assert response['data']['task_id'] == task_id
-	# 	assert response['data']['result'] == None
-	# 	time.sleep(0.2)
-	# 	assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == callbacks.double(float(3 + 4))
-	# 	assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:message')
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:caller_ip')
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:host_addr')
-	# 	assert self.redis.exists(f'ARPCP:task:{task_id}:callback')
-	# 	assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
-	# 	assert task_id in json.loads(self.redis.get('ARPCP:tasks:execute'))
-	# 	arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+	def test_result_method_for_unassigned_task(self):
+		task_id = str(uuid.uuid4())
+		try:
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		except:
+			assigned_tasks = []
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		response = arpcp.ARPCP.call(self.host, self.port, 'result', {
+			'task_id': task_id,
+			'task_status': 'done',
+			'task_result': float(5)
+		})
+		assert response['code'] == 204
+		assert response['description'] == 'unknown assign task'
+		assert response['data'] == None
+		time.sleep(0.2)
+		assert not self.redis.exists(f'ARPCP:task:{task_id}:result')
+		assert not self.redis.exists(f'ARPCP:task:{task_id}:status')
+		assert not task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
 
+
+	def test_signal_method(self):
+		time.sleep(1)
+		task_id = str(uuid.uuid4())
+		if self.redis.exists('ARPCP:tasks:execute'):
+			executed_tasks = json.loads(self.redis.get('ARPCP:tasks:execute'))
+		else:
+			executed_tasks = []
+		executed_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:execute', json.dumps(executed_tasks))
+		if self.redis.exists('ARPCP:tasks:assign'):
+			assigned_tasks = json.loads(self.redis.get('ARPCP:tasks:assign'))
+		else:
+			assigned_tasks = []
+		assigned_tasks.append(task_id)
+		self.redis.set('ARPCP:tasks:assign', json.dumps(assigned_tasks))
+		self.redis.set(f"ARPCP:task:{task_id}:status", 'done')
+		self.redis.set(f"ARPCP:task:{task_id}:result", json.dumps(12))
+		self.redis.set(f"ARPCP:task:{task_id}:callback", 'double')
+		response = arpcp.ARPCP.call(self.host, self.port, 'signal', {
+			'task_id': task_id
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data'] == 'OK'
+		time.sleep(0.2)
+		assert json.loads(self.redis.get(f"ARPCP:task:{task_id}:result")) == float(24)
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+
+
+	def test_signal_method_with_wrong_task_id(self):
+		task_id = str(uuid.uuid4())
+		response = arpcp.ARPCP.call(self.host, self.port, 'signal', {
+			'task_id': task_id * 2
+		})
+		assert response['code'] == 205
+		assert response['description'] == 'unknown execute task'
+		assert response['data'] == None
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+
+
+	def test_atask_method_wo_callback(self):
+		task_id = str(uuid.uuid4())
+		response = arpcp.ARPCP.call(self.host, self.port, 'atask', {
+			'remote_procedure': 'add',
+			'remote_procedure_args': [3, 4],
+			'task_id': task_id
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data']['task_id'] == task_id
+		assert response['data']['result'] == None
+		time.sleep(0.2)
+		assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == float(3 + 4)
+		assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
+		assert self.redis.exists(f'ARPCP:task:{task_id}:message')
+		assert self.redis.exists(f'ARPCP:task:{task_id}:caller_ip')
+		assert self.redis.exists(f'ARPCP:task:{task_id}:host_addr')
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:execute'))
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
+
+
+	def test_atask_method(self):
+		import callbacks
+		task_id = str(uuid.uuid4())
+		response = arpcp.ARPCP.call(self.host, self.port, 'atask', {
+			'remote_procedure': 'add',
+			'remote_procedure_args': [3, 4],
+			'task_id': task_id
+		}, additions = {
+			'callback': 'double'
+		})
+		assert response['code'] == 100
+		assert response['description'] == 'OK'
+		assert response['data']['task_id'] == task_id
+		assert response['data']['result'] == None
+		time.sleep(0.2)
+		assert json.loads(self.redis.get(f'ARPCP:task:{task_id}:result')) == callbacks.double(float(3 + 4))
+		assert self.redis.get(f'ARPCP:task:{task_id}:status') == 'done'
+		assert self.redis.exists(f'ARPCP:task:{task_id}:message')
+		assert self.redis.exists(f'ARPCP:task:{task_id}:caller_ip')
+		assert self.redis.exists(f'ARPCP:task:{task_id}:host_addr')
+		assert self.redis.exists(f'ARPCP:task:{task_id}:callback')
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:assign'))
+		assert task_id in json.loads(self.redis.get('ARPCP:tasks:execute'))
+		arpcp.ARPCP.erase_task_from_redis(self.redis, task_id)
